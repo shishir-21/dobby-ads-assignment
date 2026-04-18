2daf49bbec4b9470cb618de145011ad1e3a7021e
Shishir Mahato <mahatoshishir6@gmail.com>
2026-04-18 05:31:08 +0530

    Add console.log to debug MONGO_URI in Render deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI:", process.env.MONGO_URI);
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
